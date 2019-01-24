e88a2b1511d92011a9ed7d0d33d41e27a7deff42
Kyle Taylor Parkins <kyle@athom.com>
2019-01-24 13:15:15 +0100

    Fixed ADB-15 and ADB-18

--- drivers/adb-15/device.js
+++ drivers/adb-15/device.js
@@ -6,7 +6,7 @@ const util = require('homey-rfdriver').util;
 module.exports = RFDevice => class ADB15Device extends Doorbell(RFDevice) {
     static generateData() {
 		const data = {
-			address: util.generateRandomBitString(17), //alecto 4 is 17bit long
+			address: util.generateRandomBitString(17), //alecto 1/4 is 17bit long
 			state: 0,
 			generated: true,
 		};
