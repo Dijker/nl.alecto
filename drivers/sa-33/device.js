fd9935e924c4b68956fe3e22215d705d608353ad
Kyle Taylor Parkins <kyle@athom.com>
2019-02-06 09:04:04 +0100

    Removed SA-33 smokedetector Updated readme

--- drivers/sa-33/device.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const SmokeDetector = require('../../lib/devices/SmokeDetector');
-
-module.exports = RFDriver => class SA33Device extends SmokeDetector(RFDriver) {};
--- drivers/sa-33/driver.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const RFDriver = require('homey-rfdriver');
-
-module.exports = class SA33Driver extends RFDriver.Driver {};
